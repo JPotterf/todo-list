1783a8692e7354a9c73a315ac1f740febcac985c
Jacob Potterf <jacob.potterf@gmail.com>
2021-12-07 11:00:29 +0200

    added components

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,19 @@
+import {FaPizzaSlice} from 'react-icons/fa';
+
 export const Header = () => {
     return(
-        <p>Header component</p>
+        <header className="header" data-testid="header">
+            <nav>
+                <div className="logo">
+                    <img src="./images/logo.png" alt="To Do Logo" />
+                </div>
+                <div className="settings">
+                    <ul>
+                        <li>+</li>
+                        <li><FaPizzaSlice/></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     );
 }
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,2 @@
+export const Sidebar = ()=> 
+    <p>Sidebar Component</p>;
