1948a4c9c9347024bfe7c546763f69975400d2ef
Jacob Potterf <jacob.potterf@gmail.com>
2021-12-08 09:35:42 +0200

    setup component structure

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,2 +1,39 @@
-export const Sidebar = ()=> 
-    <p>Sidebar Component</p>;
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
+
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 Days</span>
+      </li>
+    </ul>
+    <div className="sidebar__middle">
+      <span>
+        <FaChevronDown />
+      </span>
+      <h2>Project</h2>
+    </div>
+    <ul className="sidepar__projects">Projects will be here</ul>
+    Project Component here
+  </div>
+);
